1b9daed97f4cd3eebbc4c244f1b43752b22f7fc9
BrainHDV <miks.max@mail.ru>
2025-01-19 11:40:52 +0300

    assets, colors, vars, icons, mixins

---
+++ public/assets/index.ts
@@ -0,0 +1,6 @@
+import BurgerMenuIcon from "@assets/action-icons/burger.svg";
+import CloseIcon from "@assets/action-icons/close.svg";
+import ArrowRTIcon from "@assets/action-icons/arrow-rt.svg";
+import Logo from "@assets/Logo.svg";
+
+export { BurgerMenuIcon, CloseIcon, ArrowRTIcon, Logo };
